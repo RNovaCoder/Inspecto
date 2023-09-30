a3a719ac527a2a502892ad9a09add3ddae01afd7
Ricardo Villa <ricardodanielvilla1@gmail.com>
2023-09-30 03:00:28 -0500

    Rutas actualizadas para el modo Dev

--- rutas_test.js
+++ rutas_test.js
@@ -1,2 +1,2 @@
-const rutas_test = ["./dist/pieza/tests/test_1.js", "./dist/pieza/tests/test_2.js"];
+const rutas_test = ["./dist/tests/Test.js", "./dist/tests/Otros.js"];
 export default rutas_test;
